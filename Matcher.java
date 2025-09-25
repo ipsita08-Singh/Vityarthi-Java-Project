22fa7f49fa710eb45dae359de2e9c53405f2f7e6
ipsita08-Singh <ipsita.24bce11465@vitbhopal.ac.in>
2025-09-25 13:54:45 +0530

    Delete Matcher.java

--- Matcher.java
+++
@@ -1,9 +0,0 @@
-package edu.ccrm.service;
-import java.util.List;
-import java.util.function.Predicate;
-
-// Minimalistic matcher interface for filtering
-@FunctionalInterface
-public interface Matcher<X> {
-    List<X> filter(Predicate<X> rule);
-}
